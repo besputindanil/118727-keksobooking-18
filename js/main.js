6d53f9f8c00689f9c1daf13b5fd67aa565fe3c5a
besputindanil <50333644+besputindanil@users.noreply.github.com>
2019-10-03 22:39:56 +0400

    Задание поля адреса, валидация

--- js/main.js
+++ js/main.js
@@ -76,7 +76,7 @@ for (var i = 0; i < ADS_COUNT; i++) {
   });
 }
 
-var mapPins = document.querySelector('.map__pins');
+// var mapPins = document.querySelector('.map__pins');
 
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
@@ -96,15 +96,31 @@ for (i = 0; i < pins.length; i++) {
   fragment.appendChild(renderPin(pins[i]));
 }
 
-mapPins.appendChild(fragment);
+// mapPins.appendChild(fragment);
 
 /* Задание: подробности */
+
+var PRIMARY_MAIN_PIN_X = 570;
+var PRIMARY_MAIN_PIN_Y = 375;
+var MAIN_PIN_WIDTH = 65;
+var MAIN_PIN_HEIGHT = 65;
+var ARROW_HEIGHT = 22;
+
+var ROOM_CAPACITY_RELATION = {
+  1: [1],
+  2: [1, 2],
+  3: [1, 2, 3],
+  100: [0]
+};
+var ROOM_CAPACITY_MESSAGE = 'Количество гостей не соответствует количеству комнат';
 
 var adForm = document.querySelector('.ad-form');
 var adFormFieldset = adForm.querySelectorAll('fieldset');
 var mapPinMain = document.querySelector('.map__pin--main');
 var map = document.querySelector('.map');
-
+var addressInput = adForm.querySelector('#address');
+var roomNumberSelect = adForm.querySelector('[name=rooms]');
+var capacitySelect = adForm.querySelector('[name=capacity]');
 
 var insertDisabled = function () {
   for (i = 0; i < adFormFieldset.length; i++) {
@@ -114,12 +130,18 @@ var insertDisabled = function () {
 
 insertDisabled();
 
+var setAddressCoordinates = function () {
+  addressInput.value = (PRIMARY_MAIN_PIN_X + Math.floor(MAIN_PIN_WIDTH / 2)) + ', ' + (PRIMARY_MAIN_PIN_Y + Math.floor(MAIN_PIN_HEIGHT / 2) + ARROW_HEIGHT);
+};
+
 var getActivePage = function () {
   map.classList.remove('map--faded');
   adForm.classList.remove('ad-form--disabled');
   for (i = 0; i < adFormFieldset.length; i++) {
     adFormFieldset[i].removeAttribute('disabled');
   }
+
+  setAddressCoordinates();
 };
 
 mapPinMain.addEventListener('mousedown', function () {
@@ -131,3 +153,13 @@ mapPinMain.addEventListener('keydown', function (evt) {
     getActivePage();
   }
 });
+
+var validateCapacity = function () {
+  var roomsNumber = roomNumberSelect.value;
+  var capacity = parseInt(capacitySelect.value, 10);
+  capacitySelect.setCustomValidity(ROOM_CAPACITY_RELATION[roomsNumber].includes(capacity) ? '' : ROOM_CAPACITY_MESSAGE);
+};
+
+roomNumberSelect.addEventListener('change', validateCapacity);
+
+capacitySelect.addEventListener('change', validateCapacity);
