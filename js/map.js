6f537f4afdfdc7735d1e6355045976ae86d650ed
besputindanil <50333644+besputindanil@users.noreply.github.com>
2019-10-23 23:25:59 +0400

    Вывод. на карту не более 5 меток. Запрограммирован фильтр тип жилья

--- js/map.js
+++ js/map.js
@@ -13,6 +13,8 @@
   var addressInput = adForm.querySelector('#address');
   var mapPinMain = document.querySelector('.map__pin--main');
   var map = document.querySelector('.map');
+  var filters = document.querySelector('.map__filters');
+  var housingType = filters.querySelector('#housing-type');
 
   var setDisabled = function () {
     for (var i = 0; i < adFormFieldset.length; i++) {
@@ -26,6 +28,41 @@
     addressInput.value = (PRIMARY_MAIN_PIN_X + Math.floor(MAIN_PIN_WIDTH / 2)) + ', ' + (PRIMARY_MAIN_PIN_Y + Math.floor(MAIN_PIN_HEIGHT / 2) + ARROW_HEIGHT);
   };
 
+  var pins = [];
+
+  var removePins = function () {
+    var mapPin = document.querySelectorAll('.map__pin');
+    for (var i = 1; i < mapPin.length; i++) {
+      window.pin.mapPins.removeChild(mapPin[i]);
+    }
+  };
+
+  var updatePins = function () {
+    var sameTypes = pins.filter(function (pin) {
+      return ((housingType.value === 'any') ? true : (pin.offer.type === housingType.value));
+    });
+    window.pin.render(sameTypes);
+  };
+
+  var onTypeChange = function () {
+    removePins();
+    updatePins();
+  };
+
+  housingType.addEventListener('change', onTypeChange);
+
+  var onLoad = function (data) {
+    pins = data;
+    updatePins();
+  };
+
+  var onError = function (errorMessage) {
+    var errorPopup = document.querySelector('#error').content.querySelector('.error');
+    var errorText = errorPopup.querySelector('.error__message');
+    errorText.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', errorPopup);
+  };
+
   var getActivePage = function () {
     map.classList.remove('map--faded');
     adForm.classList.remove('ad-form--disabled');
@@ -33,6 +70,7 @@
       adFormFieldset[i].removeAttribute('disabled');
     }
     setAddressCoords();
+    window.backend.load(onLoad, onError);
   };
 
   mapPinMain.addEventListener('mousedown', function () {
